3b32459e62941d634628caa972c0c6d142eaf26d
Kanak Tanwar <kanaktanwarpro@gmail.com>
2024-07-13 21:32:50 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,82 @@
+from typing import Optional
+from fastapi import FastAPI, Response
+from fastapi import HTTPException
+from fastapi import status
+from pydantic import BaseModel
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [
+    {
+        "id": 1,
+        "title": "Post 1",
+        "content": "This is the content of post 1"
+    }, 
+    {
+        "id": 2, 
+        "title": "Post 2", 
+        "content": "This is the content of post 2"
+    }
+]
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = len(my_posts) + 1
+    my_posts.append(post_dict)
+    return {
+        "data": post_dict
+    }
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    try:
+        post = my_posts[id - 1]
+    except Exception:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"error": "Post not found"}
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND, 
+            detail = "Post not found"
+        )
+
+    return {"data": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    try:
+        my_posts.pop(id - 1)
+    except Exception:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Post not found"
+        )
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    try:
+        my_posts[id - 1] = post.model_dump()
+    except Exception:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Post not found"
+        )
+    
+    return {"data": my_posts[id - 1]}
