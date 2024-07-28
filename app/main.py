c3b0c2faf8d3d2b6e64bbcdc0a4eba104aa4cbcb
Kanak Tanwar <kanaktanwarpro@gmail.com>
2024-07-28 20:05:38 +0530

    commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI, Response
-from fastapi import HTTPException
-from fastapi import status
+
+from fastapi import FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
+
 
 class Post(BaseModel):
     title: str
@@ -10,17 +10,10 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+
 my_posts = [
-    {
-        "id": 1,
-        "title": "Post 1",
-        "content": "This is the content of post 1"
-    }, 
-    {
-        "id": 2, 
-        "title": "Post 2", 
-        "content": "This is the content of post 2"
-    }
+    {"id": 1, "title": "Post 1", "content": "This is the content of post 1"},
+    {"id": 2, "title": "Post 2", "content": "This is the content of post 2"},
 ]
 
 app = FastAPI()
@@ -30,18 +23,19 @@ app = FastAPI()
 def root():
     return {"message": "Hello World"}
 
+
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
 
+
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
-    post_dict['id'] = len(my_posts) + 1
+    post_dict["id"] = len(my_posts) + 1
     my_posts.append(post_dict)
-    return {
-        "data": post_dict
-    }
+    return {"data": post_dict}
+
 
 @app.get("/posts/{id}")
 def get_post(id: int):
@@ -51,11 +45,11 @@ def get_post(id: int):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"error": "Post not found"}
         raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND, 
-            detail = "Post not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
         )
 
     return {"data": post}
+
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
@@ -63,11 +57,11 @@ def delete_post(id: int):
         my_posts.pop(id - 1)
     except Exception:
         raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail = "Post not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
         )
-    
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
@@ -75,8 +69,7 @@ def update_post(id: int, post: Post):
         my_posts[id - 1] = post.model_dump()
     except Exception:
         raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail = "Post not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
         )
-    
+
     return {"data": my_posts[id - 1]}
